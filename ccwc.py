876f102595d676e1f41da69452ac598cab357772
Matthew Johnson <mtthw.jhnson@gmail.com>
2024-06-04 19:46:46 -0500

    Added abilty to check byte size of the file with import os. will need to refactor for next requirement

--- ccwc.py
+++ ccwc.py
@@ -1,11 +1,22 @@
+import sys
+import functools
+import os
+
 def main():
-    book_path="test.txt"
-    file_text=file_open(book_path)
-    print(file_text)
-    
-def file_open(book_path):
-    with open(book_path) as f:
-        file_contents = f.read()
-        return file_contents
+    arguments=sys.argv[1:]
+    text_path="test.txt"
+    text_stats=os.stat(text_path)
+    print(text_stats.st_size)
+    # file_text=file_open(text_path)
+    # count=byte_count(file_text)
+    # print(count)
+
+# def byte_count(file_text):
+#     return functools.reduce(lambda accum, file_text: ord(accum) + ord(file_text), file_text)
+
+# def file_open(text_pah):
+#     with open(text_path) as f:
+#         file_contents = f.read()
+#         return file_contents
 
 main()
