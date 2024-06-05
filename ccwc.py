367f9b6de4cca3386e5f3daf0c4ac22bdd93eec8
Matthew Johnson <mtthw.jhnson@gmail.com>
2024-06-04 20:08:13 -0500

    finished part 2

--- ccwc.py
+++ ccwc.py
@@ -1,22 +1,23 @@
 import sys
-import functools
 import os
 
 def main():
     arguments=sys.argv[1:]
     text_path="test.txt"
-    text_stats=os.stat(text_path)
-    print(text_stats.st_size)
-    # file_text=file_open(text_path)
-    # count=byte_count(file_text)
-    # print(count)
+    full_text=file_open(text_path)
+    if arguments[0] == "-c":
+        text_stats=os.stat(text_path)
+        print(text_stats.st_size)
+    elif arguments[0] == "-l":
+        lines=0
+        for text in full_text:
+            if text=="\n":
+                lines= lines + 1
+        print(lines)
 
-# def byte_count(file_text):
-#     return functools.reduce(lambda accum, file_text: ord(accum) + ord(file_text), file_text)
-
-# def file_open(text_pah):
-#     with open(text_path) as f:
-#         file_contents = f.read()
-#         return file_contents
+def file_open(text_path):
+    with open(text_path) as f:
+        file_contents = f.read()
+        return file_contents
 
 main()
